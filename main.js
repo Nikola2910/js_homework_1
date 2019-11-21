4bb8d0498c19e3a10f93154594f22272bdc3a85b
Nikola2910 <nikolaivanovic2910@gmail.com>
2019-11-21 16:08:52 +0100

    first homework final

---
+++ main.js
@@ -0,0 +1,33 @@
+//TASK 1
+var PetersMoney, JimsMoney;
+
+PetersMoney = 3500;
+JimsMoney = 5000;
+
+console.log(PetersMoney > JimsMoney);
+
+//TASK 2
+var John, future, Mike;
+
+John = 38;
+future = John + 8;
+Mike = future / 2;
+
+console.log(Mike);
+
+//TASK 3
+var flockOne, flockTwo, move;
+
+flockOne = 78;
+flockTwo = 57;
+move = 12;
+
+flockOne -= move;
+flockTwo += move;
+
+flockTwo -= 4;
+
+// console.log(flockTwo);
+// console.log(flockOne);
+
+console.log(flockOne > flockTwo);
