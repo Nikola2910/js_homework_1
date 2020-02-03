414e0212b0ae41de6114851839be43c9e313bb2a
Nikola Ivanovic <nikolaivanovic2910@gmail.com>
2020-02-03 15:27:37 +0100

    making simple api request for changing dog breeds

---
+++ JS 13 - AJAX/main.js
@@ -0,0 +1,58 @@
+//Global var-s
+
+var body = document.querySelector("body");
+var imgDiv = document.querySelector("#image-div");
+var select = document.querySelector("select");
+var breed = select.value;
+
+select.addEventListener("change", changeBreed);
+
+//Defining function for changing select value
+
+function changeBreed() {
+  breed = select.value;
+  if (select.options.length === 6) {
+    select.removeChild(select.options[0]);
+  }
+  request();
+}
+
+//Sending XMLHttp Request and calling functions
+
+function request() {
+  var newRequest = new XMLHttpRequest();
+  newRequest.open(
+    "GET",
+    "https://dog.ceo/api/breed/" + breed + "/images/random"
+  );
+
+  newRequest.onload = function() {
+    var imgSrc = JSON.parse(newRequest.responseText).message;
+    var imgs = imgDiv.childNodes.length;
+
+    if (imgs === 0) {
+      createImg(imgSrc);
+    } else {
+      changeImg(imgSrc);
+    }
+  };
+
+  newRequest.send();
+}
+
+//Defining functions for creating/changing images
+
+function createImg(imgSrc) {
+  var img = document.createElement("img");
+  img.setAttribute("src", imgSrc);
+
+  imgDiv.appendChild(img);
+}
+
+function changeImg(imgSrc) {
+  imgDiv.querySelector("img").removeAttribute("src");
+  imgDiv.querySelector("img").setAttribute("src", imgSrc);
+}
+
+request();
+setInterval(request, 5000);
